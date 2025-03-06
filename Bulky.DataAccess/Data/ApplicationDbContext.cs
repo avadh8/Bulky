4c0868affbbe4eb52ce5b0e31d3fa9dbc8030ef3
avadh8 <avadhbhesaniya8@gmail.com>
2025-03-06 12:30:12 +0530

    N-tire architecture

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,8 +1,8 @@
 ﻿
-using BulkyWeb.Models;
+using Bulky.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyWeb.Data
+namespace Bulky.DataAccess.Data
 {
     public class ApplicationDbContext : DbContext
     {
